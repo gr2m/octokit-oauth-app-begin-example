b49ec12752c9b9ae404f176b0aeacbd6dd5b97a2
Gregor Martynus <gregor@martynus.net>
2020-02-27 15:23:52 -0800

    build: install @octokit/oauth-app for each function, instead of root directory

--- src/http/delete-api-github-oauth-token/index.js
+++ src/http/delete-api-github-oauth-token/index.js
@@ -1,16 +1,42 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+const arc = require("@architect/functions");
+const { deleteToken } = require("@octokit/oauth-app");
 
-exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    status: 200,
-    headers: {
-      'content-type': 'application/json; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body: JSON.stringify({ok: true})
+exports.handler = async function http(request) {
+  try {
+    const body = arc.http.helpers.bodyParser(request);
+    await deleteToken({
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      state: body.state,
+      code: body.code
+    });
+
+    return {
+      statusCode: 200
+    };
+  } catch (error) {
+    const headers = {
+      "content-type": "application/json; charset=utf8",
+      "cache-control":
+        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0"
+    };
+
+    if (/The code passed is incorrect or expired/i.test(error.message)) {
+      return {
+        statusCode: 400,
+        headers,
+        body: JSON.stringify({
+          error: error.message
+        })
+      };
+    }
+
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({
+        error: "Server error"
+      })
+    };
   }
-}
+};
