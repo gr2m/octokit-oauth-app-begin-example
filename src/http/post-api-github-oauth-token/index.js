05ec2149c7bcf602023aa709a5c28507017f83ad
Gregor Martynus <gregor@martynus.net>
2020-02-27 14:40:13 -0800

    feat: `POST /api/github/oauth/token`

--- src/http/post-api-github-oauth-token/index.js
+++ src/http/post-api-github-oauth-token/index.js
@@ -1,15 +1,36 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+const arc = require("@architect/functions");
+const { createToken } = require("@octokit/oauth-app");
 
-exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    status: 302,
-    headers: {
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    location: '/'
+exports.handler = async function http(request) {
+  const body = arc.http.helpers.bodyParser(request);
+
+  try {
+    const { token, scopes } = await createToken({
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      state: body.state,
+      code: body.code
+    });
+
+    return {
+      statusCode: 201,
+      body: { token, scopes }
+    };
+  } catch (error) {
+    if (/The code passed is incorrect or expired/i.test(error.message)) {
+      return {
+        statusCode: 400,
+        body: {
+          error: error.message
+        }
+      };
+    }
+
+    return {
+      statusCode: 500,
+      body: {
+        error: "Server error"
+      }
+    };
   }
-}
+};
